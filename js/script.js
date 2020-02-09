8663ee7a22b1e113bef541d97187062c563c20d8
Karol Makuła <karol.makula@gmail.com>
2020-02-09 11:43:10 +0100

    Add conditional statements for playerMove

--- js/script.js
+++ js/script.js
@@ -32,6 +32,10 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
 	playerMove = 'kamień';
+} else if(playerInput == '2'){
+	playerMove = 'papier';
+} else if(playerInput == '3'){
+	playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
