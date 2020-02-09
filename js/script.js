b5faf5814c29f23ba03bfc1ec7ea3671e2a42d9a
Karol Makuła <karol.makula@gmail.com>
2020-02-09 11:26:15 +0100

    Add conditional statements for computerMove

--- js/script.js
+++ js/script.js
@@ -8,8 +8,30 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let computerMove = "kamień";
+let randomNumber = Math.floor(Math.random() * 3 +1);
 
-let playerMove = "papier";
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage("Zagrałem " + computerMove + "! Jeśli Twój ruch to " + playerMove + ", to wygrywasz!");
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1){
+	computerMove = 'kamień';
+} else if  (randomNumber == 2){
+	computerMove = 'papier';
+} else if (randomNumber == 3){
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz sój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+	playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
