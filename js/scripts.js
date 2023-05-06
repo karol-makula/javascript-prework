f58539c801c0e51284d007bc67f19b2cfdaf2a97
Karol Makuła <karol.makula@gmail.com>
2023-05-06 22:27:11 +0200

    Add games results

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ else{
     computerMove = 'nożyce';
 }
 
-printMessage('Mój ruch to: ' + computerMove + '!');
+printMessage('Wybór komputera to: ' + computerMove + '!');
 
 let playerMove = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
 
@@ -29,4 +29,29 @@ else {
     playerMove = "nieznany ruch";
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Wybór gracza to: ' + playerMove + '!');
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Gracz wygrywa!');
+}
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Komputer wygrywa!');
+}
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+}
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Gracz wygrywa!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Gracz wygrywa!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Komputer wygrywa!');
+}
+else if(playerMove == 'nieznany ruch'){
+    printMessage('Wpisz liczbę z zakresu 1-3');
+}
+else{
+    printMessage('Mamy remis');
+}
